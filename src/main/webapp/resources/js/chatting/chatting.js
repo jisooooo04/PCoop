8df461293e0347ad1fe6b76dc0f8db79e87ce2e6
jisoooo <jisooooo04@gmail.com>
2020-07-20 11:57:15 +0900

    백업 - 파일 삭제된 후, 다운로드 시도 처리

--- src/main/webapp/resources/js/chatting/chatting.js
+++ src/main/webapp/resources/js/chatting/chatting.js
@@ -2,13 +2,12 @@ $(function(){
       updateScroll();
       updateFileScroll();
       
-      var ws = new WebSocket("ws://localhost/chat");  //이 url에 소켓 연결을 요청하고, WebChat 클래스가 요청을 받음
+      var ws = new WebSocket("ws://192.168.60.18/chat");  //이 url에 소켓 연결을 요청하고, WebChat 클래스가 요청을 받음
       
       ws.onmessage = function(e){
          
          var msg = JSON.parse(e.data);
          
-         alert(""+msg.c_num + $(".chat_title").attr("id"))
          if(msg.c_num != $(".chat_title").attr("id")){
         	 return false;
          }
